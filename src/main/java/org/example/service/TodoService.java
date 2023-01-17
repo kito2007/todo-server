33e6b9d78774e0610f8beee250622ad7754e1f9d
kito2007 <kito200742@gmail.com>
2023-01-17 18:41:08 +0900

    TodoController(create), TodoService test code 작성완

--- src/main/java/org/example/service/TodoService.java
+++ src/main/java/org/example/service/TodoService.java
@@ -15,7 +15,8 @@ import java.util.List;
 public class TodoService {
 
     private final TodoRepository todoRepository;
-    public TodoEntity add(TodoRequest request){
+
+    public TodoEntity add(TodoRequest request) {
         TodoEntity todoEntity = new TodoEntity();
         todoEntity.setTitle(request.getTitle());
         todoEntity.setOrder(request.getOrder());
@@ -23,33 +24,33 @@ public class TodoService {
         return this.todoRepository.save(todoEntity);
     }
 
-    public TodoEntity searchById(Long id){
+    public TodoEntity searchById(Long id) {
         return this.todoRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
-    public List<TodoEntity> searchAll(){
+    public List<TodoEntity> searchAll() {
         return this.todoRepository.findAll();
     }
 
-    public TodoEntity updateById(Long id, TodoRequest request){
+    public TodoEntity updateById(Long id, TodoRequest request) {
         TodoEntity todoEntity = this.searchById(id);
-        if (request.getTitle() != null){
+        if (request.getTitle() != null) {
             todoEntity.setTitle(request.getTitle());
         }
-        if (request.getOrder() != null){
+        if (request.getOrder() != null) {
             todoEntity.setOrder(request.getOrder());
         }
-        if (request.getCompleted() != null){
+        if (request.getCompleted() != null) {
             todoEntity.setCompleted((request.getCompleted()));
         }
         return this.todoRepository.save(todoEntity);
     }
 
-    public void deleteById(Long id){
+    public void deleteById(Long id) {
         this.todoRepository.deleteById(id);
     }
 
-    public void deleteAll(){
+    public void deleteAll() {
         this.todoRepository.deleteAll();
     }
 }
